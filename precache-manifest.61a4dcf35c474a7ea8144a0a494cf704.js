6db2896bbf537c79e64175ba5bd6ee9d2eb83482
huy <viethuy51301515@gmail.com>
2020-05-21 17:22:35 +0700

    Updates

---
+++ precache-manifest.61a4dcf35c474a7ea8144a0a494cf704.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e8ac6cef8076b5aa84830c675f52e2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d1d541f94d3af8cece18",
+    "url": "/portfolio/static/css/main.759ffe30.chunk.css"
+  },
+  {
+    "revision": "ad154cc4bba5d0436ab7",
+    "url": "/portfolio/static/js/2.693e67d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.693e67d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d541f94d3af8cece18",
+    "url": "/portfolio/static/js/main.9332e126.chunk.js"
+  },
+  {
+    "revision": "e7fea7fb795252924974",
+    "url": "/portfolio/static/js/runtime-main.dcc1eea5.js"
+  },
+  {
+    "revision": "3b85f267734f0919f777e7e0453821c4",
+    "url": "/portfolio/static/media/cloth.3b85f267.jpg"
+  },
+  {
+    "revision": "f9efa3c29bcce2c395d079bd744babf1",
+    "url": "/portfolio/static/media/hinhnen.f9efa3c2.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  }
+]);
