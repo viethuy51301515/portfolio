f61b45f1591f477175b6e96fb552eb0b6b27de95
huy <viethuy51301515@gmail.com>
2020-05-22 17:06:44 +0700

    '123'

--- profolio/src/component/contact/contact.js
+++ profolio/src/component/contact/contact.js
@@ -9,11 +9,38 @@ class Contact extends React.Component{
         return(
             <div className='main-page'>
                  <Title title='Contact'></Title>
-                 <div>
-                     <section>
+                 <div className='contact'>
+                     <section className='contact__form'>
                         <h1>Get In Touch</h1>
+                        <div className='form__item'>
+                            <label>
+                                Enter your name
+                            </label>
+                            <input id='name' name='name'/>
+                        </div>
+                        <div className='form__item'>
+                        <label>
+                                Enter your name
+                            </label>
+                            <input id='name' name='name'/>
+                        </div>
+                        <div className='form__item'>
+                            <label>
+                                Enter your name
+                            </label>
+                            <input id='name' name='name'/>
+                        </div>
                      </section>
-                     <section>
+                     <section className='contact__infor'>
+                        <div className='infor__item'>
+                            <div>
+                                <span className='icon-mobile-phone'></span>
+                            </div>
+                            <div>
+                                <h1>Phone</h1>
+                                <p>01264895603</p>
+                            </div>
+                        </div>
                         
                      </section>
                  </div>
