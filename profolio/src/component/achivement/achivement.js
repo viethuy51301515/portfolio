7d34408ab170619891266a0b21eb733578ffcd43
huy <viethuy51301515@gmail.com>
2020-05-28 16:41:05 +0700

    '123'

--- profolio/src/component/achivement/achivement.js
+++ profolio/src/component/achivement/achivement.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import './achivement.scss'
 import Title from '../title_header';
+const img = require('../../image/html.png')
 class Achivement extends React.Component{
     constructor(props){
         super(props)
     }
     render(){
+        var items = [];
+        for (let index = 0; index < 4; index++) {
+            items.push(
+                <section className='achivement-item'>
+                        <div className='achivement-item__info'>    
+                            <article>
+                                <p>Achivement</p>
+                                <p>Customizing Colors and Fonts</p>
+                                <p>How to make the websites</p>
+                            </article>
+                            <article>
+                                <span style={{color:'5fcf80'}} className='icon-checkmark-circle'></span>
+                                <aside>
+                                    <p>Achieved</p>
+                                    <span>Nov 18, 2014</span>
+                                </aside>
+                            </article>
+
+                        </div>
+                        <div className='achivement-item__img'>
+                            <figure>
+                                <img src={img}></img>
+                            </figure>
+                        </div>
+                </section>
+            )
+        }
         return(
             <div className='main-page'>
                 <Title title='Achivement'></Title>
                 <div className='achivement-grid'>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    {items}
+                   
                 </div>
             </div>
         )
