598246c2b2d34cf7bcd81ba1fe67d514bf0f25a9
viethuy <viethuy51301515@gmail.com>
2020-06-02 07:27:10 +0700

    '123'

--- profolio/src/component/achivement/achivement.js
+++ profolio/src/component/achivement/achivement.js
@@ -4,35 +4,51 @@ import Title from '../title_header';
 const img = require('../../image/html.png')
 class Achivement extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            fliped : false
+        }
+    }
+    openImg(){
+        this.setState({
+            fliped:!this.state. fliped
+        })
     }
     render(){
         var items = [];
+        var style = {
+            backgroundImage:`url(${img})`
+        }
+        var classN = `achivement-img ${this.state.fliped ? 'achivement-img--fliped' : ''}`;
+        var classM = `achivement-item ${this.state.fliped ? 'achivement-img--fliped' : ''}`;
         for (let index = 0; index < 4; index++) {
             items.push(
                 <div>
-                <section className='achivement-item'>
-                        <div className='achivement-item__info'>    
-                            <article>
-                                <p>Achivement</p>
-                                <p>Customizing Colors and Fonts</p>
-                                <p>How to make the websites</p>
-                            </article>
-                            <article>
-                                <span style={{color:'5fcf80'}} className='icon-checkmark-circle'></span>
-                                <aside>
-                                    <p>Achieved</p>
-                                    <span>Nov 18, 2014</span>
-                                </aside>
-                            </article>
+                    <section className={classM} onClick={this.openImg.bind(this)}>
+                            <div className='achivement-item__info'>    
+                                <article>
+                                    <p>Achivement</p>
+                                    <p>Customizing Colors and Fonts</p>
+                                    <p>How to make the websites</p>
+                                </article>
+                                <article>
+                                    <span style={{color:'5fcf80'}} className='icon-checkmark-circle'></span>
+                                    <aside>
+                                        <p>Achieved</p>
+                                        <span>Nov 18, 2014</span>
+                                    </aside>
+                                </article>
 
-                        </div>
-                        <div className='achivement-item__img'>
-                            <figure>
-                                <img src={img}></img>
-                            </figure>
-                        </div>
-                </section>
+                            </div>
+                            <div className='achivement-item__img'>
+                                <figure>
+                                    <img src={img}></img>
+                                </figure>
+                            </div>
+                    </section>
+                    <div className={classN} style={style}>
+                        
+                    </div>
                 </div>
             )
         }
