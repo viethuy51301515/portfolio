2fa4dfc85e8fe49d4f5cd67ce02d4a564c0ef2d2
huy <viethuy51301515@gmail.com>
2020-07-07 17:32:37 +0700

    '123'

--- profolio/src/component/achivement/achivement.js
+++ profolio/src/component/achivement/achivement.js
@@ -1,66 +1,61 @@
 import React from 'react';
+import {useState} from "react";
 import './achivement.scss'
 import Title from '../title_header';
-const img = require('../../image/html.png')
-class Achivement extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            fliped : false
-        }
+const img = require('../../image/html.png');
+const Achivement = (props) =>{
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         fliped : false
+    //     }
+    // }
+    const [fliped,setFliped] = useState(false);
+    var items = [];
+    var style = {
+        backgroundImage:`url(${img})`
     }
-    openImg(){
-        this.setState({
-            fliped:!this.state. fliped
-        })
-    }
-    render(){
-        var items = [];
-        var style = {
-            backgroundImage:`url(${img})`
-        }
-        var classN = `achivement-img ${this.state.fliped ? 'achivement-img--fliped' : ''}`;
-        var classM = `achivement-item ${this.state.fliped ? 'achivement-img--fliped' : ''}`;
-        for (let index = 0; index < 4; index++) {
-            items.push(
-                <div>
-                    <section className={classM}>
-                            <div className='achivement-item__info'>    
-                                <article>
-                                    <p>Achivement</p>
-                                    <p>Customizing Colors and Fonts</p>
-                                    <a href='https://www.freecodecamp.org/certification/viethuy/front-end-libraries' target='_blank'>How to make the websites</a>
-                                </article>
-                                <article>
-                                    <span style={{color:'5fcf80'}} className='icon-checkmark-circle'></span>
-                                    <aside>
-                                        <p>Achieved</p>
-                                        <span>Nov 18, 2014</span>
-                                    </aside>
-                                </article>
+    var classN = `achivement-img ${fliped ? 'achivement-img--fliped' : ''}`;
+    var classM = `achivement-item ${fliped? 'achivement-img--fliped' : ''}`;
+    for (let index = 0; index < 4; index++) {
+        items.push(
+            <div>
+                <section className={classM}>
+                        <div className='achivement-item__info'>    
+                            <article>
+                                <p>Achivement</p>
+                                <p>Customizing Colors and Fonts</p>
+                                <a href='https://www.freecodecamp.org/certification/viethuy/front-end-libraries' target='_blank'>How to make the websites</a>
+                            </article>
+                            <article>
+                                <span style={{color:'5fcf80'}} className='icon-checkmark-circle'></span>
+                                <aside>
+                                    <p>Achieved</p>
+                                    <span>Nov 18, 2014</span>
+                                </aside>
+                            </article>
 
-                            </div>
-                            <div className='achivement-item__img'>
-                                <figure>
-                                    <img src={img}></img>
-                                </figure>
-                            </div>
-                    </section>
-                    <div className={classN} style={style}>
-                        
-                    </div>
-                </div>
-            )
-        }
-        return(
-            <div className='main-page'>
-                <Title title='Achivement'></Title>
-                <div className='achivement-grid'>
-                    {items}
-                   
+                        </div>
+                        <div className='achivement-item__img'>
+                            <figure>
+                                <img src={img}></img>
+                            </figure>
+                        </div>
+                </section>
+                <div className={classN} style={style}>
+                    
                 </div>
             </div>
         )
     }
+    return(
+        <div className='main-page'>
+            <Title title='Achivement'></Title>
+            <div className='achivement-grid'>
+                {items}
+                
+            </div>
+        </div>
+    )
 }
 export default Achivement;
