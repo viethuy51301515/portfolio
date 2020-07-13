0751114cf64688645d9a9088c71111cd4b2d08ea
viethuy <viethuy51301515@gmail.com>
2020-07-13 22:18:39 +0700

    Updates

---
+++ precache-manifest.0669982ef138bd4fc7678a38cc0dd5a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d50b6134de4f5cade1b7a4056159a4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "44ff23679407fe7d93b0",
+    "url": "/portfolio/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "f3716f12d117de688370",
+    "url": "/portfolio/static/css/main.8b3a98f2.chunk.css"
+  },
+  {
+    "revision": "44ff23679407fe7d93b0",
+    "url": "/portfolio/static/js/2.7eba8dd9.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/portfolio/static/js/2.7eba8dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3716f12d117de688370",
+    "url": "/portfolio/static/js/main.954b4942.chunk.js"
+  },
+  {
+    "revision": "e7fea7fb795252924974",
+    "url": "/portfolio/static/js/runtime-main.dcc1eea5.js"
+  },
+  {
+    "revision": "3b85f267734f0919f777e7e0453821c4",
+    "url": "/portfolio/static/media/cloth.3b85f267.jpg"
+  },
+  {
+    "revision": "f9efa3c29bcce2c395d079bd744babf1",
+    "url": "/portfolio/static/media/hinhnen.f9efa3c2.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8c5c6346d948f82251ca7a7893f9e1a7.js"
+  "/portfolio/precache-manifest.0669982ef138bd4fc7678a38cc0dd5a2.js"
 );
 
 self.addEventListener('message', (event) => {
