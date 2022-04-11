9cf722d5a13f01e1b35e1828fa74fdbff530454f
viethuy51301515 <viethuy51301515@gmail.com>
2022-04-11 15:22:30 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a2d9bb83db578bc60f46cdf7a89591f1.js"
+  "/portfolio/precache-manifest.7245aa457605a2af06cc3de8a2a6232c.js"
 );
 
 self.addEventListener('message', (event) => {
